1d4fd999a7ca2531b08200c137488d7f456ceb5b
melchor629 <melchor9000@gmail.com>
2019-08-16 21:32:39 +0200

    Fix year in footer

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -17,7 +17,7 @@ export default () => (
         <div className="row">
             <div className="col col-sm-auto text-center text-sm-right">
                 <p>
-                    melchor629/melchor9000 (2018)
+                    melchor629/melchor9000 ({new Date().getFullYear()})
                 </p>
             </div>
 
