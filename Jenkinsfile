b61c698372f9c332714f504ba347ede59efbe0e2
melchor629 <melchor9000@gmail.com>
2019-12-22 19:19:37 +0100

    refactor: remove unnecessary config from function configuration

--- Jenkinsfile
+++ Jenkinsfile
@@ -45,6 +45,7 @@ pipeline {
 
       steps {
         sh 'npm run build'
+        sh 'npm --prefix functions run build'
       }
     }
 
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,11 +2,7 @@ import * as firebase from 'firebase-admin'
 import * as functions from 'firebase-functions'
 import postsApp from './posts'
 
-firebase.initializeApp({
-  credential: firebase.credential.applicationDefault(),
-  databaseURL: 'https://melchor9000-webpage.firebaseio.com',
-  storageBucket: 'gs://melchor9000-webpage.appspot.com',
-})
+firebase.initializeApp()
 
 const euFunctions = functions.region('europe-west1')
 
