cec60d41d585c84498beeac1563d58d1c89799b6
melchor629 <melchor9000@gmail.com>
2020-12-23 20:51:16 +0100

    feature: use node 12

--- functions/src/utils/decorators/handler-catch.ts
+++ functions/src/utils/decorators/handler-catch.ts
@@ -1,7 +1,7 @@
 import { RequestHandler } from 'express'
 
 const handlerCatch = (func: RequestHandler): RequestHandler => (req, res, next) => {
-  const returnValue = func(req, res, next)
+  const returnValue = func(req, res, next) as Promise<void> | void
   if (returnValue instanceof Promise) {
     returnValue.catch((e) => next(e))
   }
