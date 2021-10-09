93ca68e228cd51d252988291a18d4642e039482d
melchor629 <melchor9000@gmail.com>
2021-10-09 17:49:15 +0200

    refac: improve code rendering in posts

--- functions/src/utils/logic/render-post.ts
+++ functions/src/utils/logic/render-post.ts
@@ -1,35 +1,42 @@
-import { Parser, HtmlRenderer } from 'commonmark'
+import { Parser, HtmlRenderer, Node } from 'commonmark'
 import * as highlight from 'highlight.js'
 
-const codeRegex = /<pre><code(?: class="language-(.+)")?>/
+function* walk(tree: Node) {
+  const walker = tree.walker()
+  let event = walker.next()
+  while (event) {
+    yield [event.node, event.entering] as const
+    event = walker.next()
+  }
+}
 
 export default (contents: string, format: 'html' | 'md') => {
   let html = contents
   if (format === 'md') {
     const reader = new Parser()
     const writter = new HtmlRenderer()
-    const tree = reader.parse(html)
-    html = writter.render(tree)
-  }
+    const tree = reader.parse(contents)
 
-  let i = 0
-  let codeMatches = codeRegex.exec(html.substr(i))
-  while (codeMatches !== null) {
-    const codeStart = i + codeMatches.index + codeMatches[0].length
-    const codeEnd = /<\/code><\/pre>/.exec(html.substr(codeStart))!.index
-    const code = html.substr(codeStart, codeEnd)
-      .replace(/&lt;/g, '<')
-      .replace(/&gt;/g, '>')
-      .replace(/&amp;/g, '&')
-      .replace(/&quot;/g, '"')
-    const codeRendered = codeMatches[1]
-      // @ts-ignore
-      ? highlight.highlight(code, { language: codeMatches[1] }).value
-      // @ts-ignore
-      : highlight.highlightAuto(code).value
-    html = html.substr(0, codeStart) + codeRendered + html.substr(codeStart + codeEnd)
-    i = codeStart + codeRendered.length
-    codeMatches = codeRegex.exec(html.substr(i))
+    const elementsToRemove: Node[] = []
+    for (const [node] of walk(tree)) {
+      if (node.type === 'code_block') {
+        const { language, value: code } = node.info
+          // @ts-ignore
+          ? highlight.highlight(node.literal, { language: node.info })
+          // @ts-ignore
+          : highlight.highlightAuto(node.literal)
+        const newNode = new Node('html_block', node.sourcepos)
+        newNode.literal = `<pre><code class="language-${language || 'unknown'}">${code}</code></pre>`
+        node.insertBefore(newNode)
+        elementsToRemove.push(node)
+      }
+    }
+
+    for (const node of elementsToRemove) {
+      node.unlink()
+    }
+
+    html = writter.render(tree)
   }
 
   return html
